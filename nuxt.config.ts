329c85c10596c835286b6554ccf7b166dc55ae1d
briofy-mammad <m.ranjbar@briofy.net>
2023-04-16 14:07:14 +0330

    transitions and input fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@morev/vue-transitions/nuxt'],
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
