7f69c0f58081b42306b4cb0655e9657eb42e5c48
Mohammad Ranjbar <129583147+briofy-mammad@users.noreply.github.com>
2023-05-17 09:47:50 +0330

    Next to main (#7)
    * [FIX] using node .output/server/index.mjs instead of nginx for SSR
    
    * [FIX] switch from port 80 to port 3000 SSR
    
    * fix ui issues and set logo
    
    * added json files
    changed nuxt config
    fixed active links
    fixed redirect to default lang
    
    * fix close menu after lang select
    added new italina content
    change in default
    
    ---------
    
    Co-authored-by: Josh <josh@briofy.net>
    Co-authored-by: briofy-mobinanaseri <m.naseri@briofy.net>

--- components/index/TabsSec.vue
+++ components/index/TabsSec.vue
@@ -3,13 +3,13 @@
     <div class="mx-auto px-4 sm:px-12 xl:max-w-6xl xl:px-0">
       <div class="text-center">
         <h2
-          class="text-3xl font-bold text-gray-800 dark:text-white md:text-4xl xl:text-5xl"
-        >
+
+          class="text-3xl font-bold text-gray-800 dark:text-white md:text-4xl xl:text-5xl">
           For growing teams and organizations
         </h2>
         <p
-          class="mx-auto mt-6 text-gray-700 dark:text-gray-300 md:w-3/4 lg:w-3/5"
-        >
+          class="mx-auto mt-6 text-gray-700 dark:text-gray-300 md:w-3/4 lg:w-3/5">
+
           A growing team doesn't need to mean growing pains. Privacy, and the
           right tool for every step of your journey - Jira Software friction -
           company size.
@@ -18,11 +18,11 @@
         <div
           role="tablist"
           aria-label="tabs"
-          class="relative mx-auto mt-12 grid h-12 w-auto grid-cols-3 items-center gap-x-1 overflow-hidden rounded-full border border-gray-200 bg-gray-100 px-[3px] text-gray-600 dark:border-gray-700 dark:border-opacity-60 dark:bg-slate-950 dark:text-gray-300 dark:shadow-none sm:w-max"
-        >
+
+          class="relative mx-auto mt-12 grid h-12 w-auto grid-cols-3 items-center gap-x-1 overflow-hidden rounded-full border border-gray-200 bg-gray-100 px-[3px] text-gray-600 dark:border-gray-700 dark:border-opacity-60 dark:bg-slate-950 dark:text-gray-300 dark:shadow-none sm:w-max">
           <div
-            class="tab-indicator absolute h-10 rounded-full bg-white shadow-md transition-[left] duration-500 dark:bg-gray-800"
-          ></div>
+            class="tab-indicator absolute h-10 rounded-full bg-white shadow-md transition-[left] duration-500 dark:bg-gray-800"></div>
+
 
           <button
             v-for="(tabItem, tabIndex) in tabBtn"
@@ -38,8 +38,9 @@
             aria-controls="panel-0"
             tabindex="0"
             title="tab item"
-            class="tab relative block rounded-full py-2.5 px-4 hover:text-blue-700 transition-alldark:hover:text-blue-500"
-          >
+
+            class="tab relative block rounded-full py-2.5 px-4 hover:text-blue-700 transition-alldark:hover:text-blue-500">
+
             <span class="m-auto block w-max text-sm font-medium tracking-wider">
               {{ tabItem }}
             </span>
@@ -50,8 +51,9 @@
       <TransitionFade>
         <IndexTabsDetail
           v-if="activeTabData && showTransition"
-          :item="activeTabData"
-        />
+
+          :item="activeTabData" />
+
       </TransitionFade>
     </div>
   </section>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,31 @@
 <template>
   <Html :lang="locale" :dir="head.htmlAttrs?.dir">
-
-  <Body>
-    <div class="min-h-screen bg-white selection:bg-primary/10 selection:text-primary dark:bg-gray-900">
-      <Header />
-      <slot />
-      <Footer />
-    </div>
-  </Body>
-
+    <Title>{{ $t("meta.title") }}</Title>
+    <Body>
+      <div
+        class="min-h-screen overflow-hidden bg-white selection:bg-primary/10 selection:text-primary dark:bg-gray-900"
+      >
+        <Header />
+        <slot />
+        <Footer />
+      </div>
+    </Body>
   </Html>
 </template>
 <script lang="ts" setup>
 //i18n
-const { locale, t } = useI18n();
+const { locale } = useI18n();
 const head = useLocaleHead({
   addDirAttribute: true,
   identifierAttribute: "id",
   addSeoAttributes: true,
 });
+
+// redirect if route was /
+const route = useRoute();
+const router = useRouter();
+const switchLocalePath = useSwitchLocalePath();
+if (route.fullPath === "/") {
+  router.replace(switchLocalePath("en"));
+}
 </script>
